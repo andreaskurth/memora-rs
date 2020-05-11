7dbd1dbd70951c8d2ef9be18d488cee85b18f586
Andreas Kurth <dev@akurth.net>
2020-05-11 21:26:23 +0200

    test_util::create_file: Simplify type requirements

--- src/test_util.rs
+++ src/test_util.rs
@@ -7,9 +7,9 @@
 use crate::error::{Error, Result};
 use std::path::Path;
 
-pub fn create_file<P: AsRef<Path> + std::fmt::Debug>(path: P) -> Result<std::fs::File> {
-    std::fs::File::create(&path)
-        .map_err(|cause| Error::chain(format!("Could not create file {:?}:", path), cause))
+pub fn create_file<P: AsRef<Path>>(path: P) -> Result<std::fs::File> {
+    std::fs::File::create(path.as_ref())
+        .map_err(|cause| Error::chain(format!("Could not create file {:?}:", path.as_ref()), cause))
 }
 
 pub fn write_file(f: &mut std::fs::File, s: &str) -> Result<()> {
