43861ca7ecc0e691a4cc72e09e206096f02f16e5
Andreas Kurth <dev@akurth.net>
2021-02-23 11:49:19 +0100

    config: Deserialize artifacts separately
    This allows to have a `name` member for each `Artifact` but still keep
    the current map syntax in the manifest.

--- src/config.rs
+++ src/config.rs
@@ -4,13 +4,14 @@
 
 //! Configuration
 
-use crate::cache::Artifacts;
+extern crate tuple_vec_map;
+use crate::cache::{Artifact, Artifacts};
 use crate::error::{Error, Result};
 use serde::Deserialize;
 use std::path::{Path, PathBuf};
 
 /// A Memora build artifact cache manifest.
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub struct Manifest {
     /// The root directory of the build artifact cache for a Git repository.
     ///
@@ -27,6 +28,21 @@ pub struct Manifest {
     /// Optional name of an environment variable that, if set, disables the cache.
     pub disable_env_var: Option<String>,
 }
+
+#[derive(Deserialize)]
+struct SerdeArtifact {
+    inputs: Vec<PathBuf>,
+    outputs: Vec<PathBuf>,
+}
+
+#[derive(Deserialize)]
+struct SerdeManifest {
+    pub cache_root_dir: PathBuf,
+    #[serde(with = "tuple_vec_map")]
+    pub artifacts: Vec<(String, SerdeArtifact)>,
+    pub disable_env_var: Option<String>,
+}
+
 impl Manifest {
     /// Load a Manifest from the file at `path`.
     ///
@@ -37,9 +53,22 @@ impl Manifest {
         let file = File::open(path)
             .map_err(|cause| Error::chain(format!("Cannot open manifest {:?}!", path), cause))?;
         let manifest = {
-            let mut manifest: Manifest = serde_yaml::from_reader(file).map_err(|cause| {
+            let serde_manifest: SerdeManifest = serde_yaml::from_reader(file).map_err(|cause| {
                 Error::chain(format!("Syntax error in manifest {:?}!", path), cause)
             })?;
+            let mut manifest = Manifest {
+                cache_root_dir: serde_manifest.cache_root_dir,
+                artifacts: serde_manifest
+                    .artifacts
+                    .iter()
+                    .map(|(name, serde_arti)| Artifact {
+                        name: name.to_string(),
+                        inputs: serde_arti.inputs.clone(),
+                        outputs: serde_arti.outputs.clone(),
+                    })
+                    .collect(),
+                disable_env_var: serde_manifest.disable_env_var,
+            };
             // Add path of Manifest to inputs of each Artifact.
             for artifact in &mut manifest.artifacts {
                 artifact.inputs.push(path.to_path_buf())
